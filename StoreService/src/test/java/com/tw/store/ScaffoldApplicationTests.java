4106165fa48d8ce662607f244debe651f4b817e7
hpan <hpan@thoughtworks.com>
2018-04-16 15:06:25 +0800

    connect store and price services to config server

--- StoreService/src/test/java/com/tw/store/ScaffoldApplicationTests.java
+++ StoreService/src/test/java/com/tw/store/ScaffoldApplicationTests.java
@@ -3,11 +3,12 @@ package com.tw.store;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = "spring.cloud.config.enabled=false")
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class ScaffoldApplicationTests {
 
